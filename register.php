47ba4b93b1e6b6950f2daeb7d773cd381c99f01f
D P Jindal <dpjindal@gmail.com>
2025-01-09 21:06:52 +0530

    Update register.php

--- register.php
+++ register.php
@@ -38,7 +38,7 @@ if (isset($_POST['register'])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Registration:Matrimony by Jiinfo</title>
+<title>Registration:Sample Matrimony by Jiinfo</title>
 </head>
 
 <body>
