1392e4ceed0751259a622bef802880ddf65924f6
D P Jindal <dpjindal@gmail.com>
2025-01-09 21:05:38 +0530

    Update login.php

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if (isset($_POST['login'])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login:Matrimony by Jiinfo</title>
+<title>Login:Sample Matrimony by Jiinfo</title>
 </head>
 
 <body>
